cb2a48d1e906b42727da92908f52099aab68e37b
Soban Mehboob <soban.mehboob@econceptions.mobi>
2026-04-29 17:34:54 +0500

    all k8 files with canary deployment

--- server/src/server.js
+++ server/src/server.js
@@ -6,6 +6,6 @@ dotenv.config();
 
 const PORT = process.env.PORT || 5001;
 await connectDB();
-app.listen(PORT, () => {
+app.listen(PORT,'0.0.0.0', () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
